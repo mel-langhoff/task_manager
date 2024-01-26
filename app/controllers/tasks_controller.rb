cb117d8a8f0310c372720da73d656c40765162de
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-26 13:58:11 -0700

    Updating tasks method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,4 +24,14 @@ class TasksController < ApplicationController
     def edit
         @task = Task.find(params[:id])
     end
+
+    def update
+        task = Task.find(params[:id])
+        task.update({
+            title: params[:title],
+            description: params[:description]
+            })
+        task.save
+        redirect_to "/tasks/#{task.id}"
+    end
 end
